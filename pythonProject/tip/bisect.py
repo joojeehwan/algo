01bc38d63b16caa3dfa0020598df56203b8b4019
jeehwan <meanstrike94@gmail.com>
2022-12-05 02:15:06 +0900

    1205 bisect 재 리뷰

--- pythonProject/tip/bisect.py
+++ pythonProject/tip/bisect.py
@@ -59,11 +59,11 @@ r = len(nums) - 1
 result = 0
 while l <= r:
     mid = (l + r) // 2
-if nums[mid] >= n:
-    result = mid
-    r = mid - 1
-else:
-    l = mid + 1
+    if nums[mid] >= n:
+        result = mid
+        r = mid - 1
+    else:
+        l = mid + 1
 
 print(result)
 
